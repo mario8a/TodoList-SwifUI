a99b78bdb787fa41e57b181003df494e37deedc2
Mario Ochoa <mario8ato@hotmail.com>
2024-09-20 16:34:30 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Mario Abraham Ochoa Tovar on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
